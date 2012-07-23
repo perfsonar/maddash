c6f11fb96b068a81cb2ee5e65b136632911885ef
andrewrlake@gmail.com <andrewrlake@gmail.com@634a38ea-2877-11de-9f0d-3bffa153b7de>
2012-07-23 17:28:39 +0000

    Fix so correct status is returned for checks that have not run yet

--- maddash-server/src/main/java/net/es/maddash/checks/CheckConstants.java
+++ maddash-server/src/main/java/net/es/maddash/checks/CheckConstants.java
@@ -11,7 +11,7 @@ public class CheckConstants {
     final static public int RESULT_WARN = 1;
     final static public int RESULT_CRITICAL = 2;
     final static public int RESULT_UNKNOWN = 3;
-    final static public int RESULT_NOTRUN = -1;
+    final static public int RESULT_NOTRUN = 4;
     final static public String EMPTY_PARAMS = "EMPTY";
     
 }
