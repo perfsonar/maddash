614202194ead71cbf0289a7771e5303337239a49
andrewrlake@gmail.com <andrewrlake@gmail.com@634a38ea-2877-11de-9f0d-3bffa153b7de>
2012-10-02 20:03:07 +0000

    Updating path in test script

--- maddash-server/bin/run.sh
+++ maddash-server/bin/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-java -Xmx256m -Djava.net.preferIPv4Stack=true -jar target/maddash-server-0.1.one-jar.jar $*
+java -Xmx256m -Djava.net.preferIPv4Stack=true -jar target/maddash-server-1.0.one-jar.jar $*
 ##
 #Use below to run JMX agent thread and memory monitoring
 #java -Xmx256m -Dmaddash.jmx.port=8080 -javaagent:target/maddash-server-0.1-jmx.jar -Djava.net.preferIPv4Stack=true -jar target/maddash-server-0.1.one-jar.jar  $*
