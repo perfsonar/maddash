cc18194babb3f8bb627d1b3438263337b2862044
andrewrlake@gmail.com <andrewrlake@gmail.com@634a38ea-2877-11de-9f0d-3bffa153b7de>
2013-12-19 16:10:22 +0000

    Reversing unintended commit

--- maddash-server/bin/run.sh
+++ maddash-server/bin/run.sh
@@ -2,5 +2,5 @@
 java -Xmx256m -Djava.net.preferIPv4Stack=true -jar target/maddash-server-1.0.one-jar.jar $*
 ##
 #Use below to run JMX agent thread and memory monitoring
-java -Dmaddash.jmx.port=8080 -javaagent:target/maddash-server-1.0-jmx.jar -Djava.net.preferIPv4Stack=true -jar target/maddash-server-1.0.one-jar.jar  $*
+#java -Xmx256m -Dmaddash.jmx.port=8080 -javaagent:target/maddash-server-0.1-jmx.jar -Djava.net.preferIPv4Stack=true -jar target/maddash-server-0.1.one-jar.jar  $*
 
