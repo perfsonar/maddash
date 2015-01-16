b53dae2c44ba639b60ec096d8d85cdc728b72449
Andy Lake <andy@es.net>
2015-01-16 13:22:25 -0500

    Fixing bug where query uses wrong column name

--- maddash-server/src/main/java/net/es/maddash/jobs/CheckSchedulerJob.java
+++ maddash-server/src/main/java/net/es/maddash/jobs/CheckSchedulerJob.java
@@ -76,7 +76,7 @@ public class CheckSchedulerJob extends Thread{
                 "c.rowName, c.colName, t.checkType, t.checkParams, t.checkInterval, " +
                 "t.retryInterval, t.retryAttempts, t.timeout, c.statusMessage FROM checkTemplates AS t, " +
                 "checks AS c WHERE c.active = 1 AND t.id = c.checkTemplateId AND " +
-                "c.nextCheckTime <= ? AND c.status != ? ORDER BY c.nextCheckTime ASC");
+                "c.nextCheckTime <= ? AND c.checkStatus != ? ORDER BY c.nextCheckTime ASC");
             selStmt.setLong(1, time);
             selStmt.setInt(2, CheckConstants.RESULT_MAINTENANCE);
             selStmt.setMaxRows(globals.getJobBatchSize());
